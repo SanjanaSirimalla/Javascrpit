cb9962a3da27fe245a2d9aec1301ab916fdc4f92
SanjanaSirimalla <163402962+SanjanaSirimalla@users.noreply.github.com>
2024-03-14 15:54:06 +0000

    done

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) //there is a diff syntax to use alert
+//we are using nodejs not browser
+
+let name='Sanjana' //string datatype
+let age=19  //number
+let isLoggedIn=true  //boolean 
+
+//primitive datatypes
+
+//number => 2 power 53
+//bigint =>large values
+//string => ""
+//boolean => true/false
+//null =>stand alone value
+//undefined =>any value is not assignmed yet
+//symbol =>to identify uniqueness of components
+
+//object
+
+console.log(typeof "soni") //string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
