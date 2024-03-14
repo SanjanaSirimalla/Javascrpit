115d508afee166016baf1999182cd21769d98360
SanjanaSirimalla <163402962+SanjanaSirimalla@users.noreply.github.com>
2024-03-14 16:22:09 +0000

    done

---
+++ basics/conversionOperation.js
@@ -0,0 +1,12 @@
+let num="33"
+console.log(typeof num);
+console.log(typeof(num));
+
+let valueInNUm=Number(num)  //to vonvert into number
+console.log(typeof valueInNUm);
+
+let num1="33abc"
+console.log(typeof num1);
+let valueInNUm1=Number(num1)
+console.log(valueInNUm1);
+console.log(typeof valueInNUm1);
