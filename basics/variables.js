7cd58f333e7ccf54863a51a139f4a5c9a39ff6ce
SanjanaSirimalla <163402962+SanjanaSirimalla@users.noreply.github.com>
2024-03-14 15:28:18 +0000

    conatiner config done

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const a=24790
+let b='uiwshj'
+var c='9610'
+d='jhigo'
+
+//constants value cannot be changed
+//a=36427
+b='4wdgdfss'
+c='4635685'
+d='dsh'
+
+console.log(a);
+console.table([a,b,c,d])
