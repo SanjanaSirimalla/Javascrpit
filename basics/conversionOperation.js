8f69d4c8f82ef121f64b9e735c2a06b9f9463f1d
SanjanaSirimalla <163402962+SanjanaSirimalla@users.noreply.github.com>
2024-03-14 16:35:06 +0000

    conversion of datatypes

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,8 +1,8 @@
 let num="33"
 console.log(typeof num);
-console.log(typeof(num));
+console.log(typeof(num));//another way of using typeof
 
-let valueInNUm=Number(num)  //to vonvert into number
+let valueInNUm=Number(num)  //to convert into number
 console.log(typeof valueInNUm);
 
 let num1="33abc"
@@ -31,7 +31,7 @@ console.log(typeof valueInNUm4);
 
 
 let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
+let booleanIsLoggedIn=Boolean(isLoggedIn)//to convert into boolean
 console.log(booleanIsLoggedIn)
 
 //1 => true ; 0 => false
@@ -39,6 +39,6 @@ console.log(booleanIsLoggedIn)
 // "any_value" => true
 
 let number=36
-let stringNumber=String(number)
+let stringNumber=String(number)//to convert into string
 console.log(stringNumber);
 console.log(typeof stringNumber);
