b93448c022474531d540df558003ec49217aacca
SanjanaSirimalla <163402962+SanjanaSirimalla@users.noreply.github.com>
2024-03-14 16:54:20 +0000

    conversions

---
+++ basics/otherConversions.js
@@ -0,0 +1,32 @@
+let number=36
+let stringNumber=String(number)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power
+// console.log(2/3);//returns decimal not 0
+// console.log(2%3);
+// console.log(5/2);
+
+let str1="hello"
+let str2=" everyone"
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);//1
+//true+ raises error
+console.log(+"");//0
+
